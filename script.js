8f9c2912c1808b88661fce6aa2b637a3b18c0155
Sabrina <rinasullivan@icloud.com>
2025-03-10 15:47:38 -0300

    New tab Updates

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const categoryLinks = document.querySelectorAll(".category h2 a");
     const categoryLinksTwo = document.querySelectorAll(".category h2");
     const headerTitle = document.querySelector(".header h1");
-    const categoryList = document.querySelectorAll(".category ul li a");
+    const searchButton = document.querySelector(".search-bar button");
 
 
     function showTab(tabId, updateUrl = true) {
@@ -16,25 +16,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // Change category link colors based on active tab
         if (tabId === "book-tab") {
-            categoryLinks.forEach(link => link.style.color = "#794e3b");
-            categoryLinksTwo.forEach(link => link.style.color = "#794e3b");
-            categoryList.forEach(link => link.style.color = "#794e3b");
-            const containers = document.querySelectorAll(".category");
-			containers.forEach(container => container.style.backgroundImage = 'url("bg-textures/tile-leather.png")');
+            categoryLinks.forEach(link => link.style.color = "#bf3d3e");
+            categoryLinksTwo.forEach(link => link.style.color = "#bf3d3e");
+            searchButton.style.backgroundColor = "#bf3d3e";
             headerTitle.textContent = "BOOKS";
         } else if (tabId === "comic-tab") {
             categoryLinks.forEach(link => link.style.color = "#6a67ad");
             categoryLinksTwo.forEach(link => link.style.color = "#6a67ad");
-            categoryList.forEach(link => link.style.color = "#787878");
-            const containers = document.querySelectorAll(".category");
-			containers.forEach(container => container.style.backgroundImage = 'url("bg-textures/tile-papergray.png")');
+            searchButton.style.backgroundColor = "#6a67ad";
             headerTitle.textContent = "COMICS";
         } else {
-            categoryLinks.forEach(link => link.style.color = "#bf3d3e");
-            categoryLinksTwo.forEach(link => link.style.color = "#bf3d3e");
-            categoryList.forEach(link => link.style.color = "#787878");
-            const containers = document.querySelectorAll(".category");
-			containers.forEach(container => container.style.backgroundImage = 'url("bg-textures/tile-paper.png")');
+            categoryLinks.forEach(link => link.style.color = "#454544");
+            categoryLinksTwo.forEach(link => link.style.color = "#454544");
+            searchButton.style.backgroundColor = "#454544";
             headerTitle.textContent = "WELCOME";
         }
 
